4fd4d01c4e2060fea4621eac931a93821a4ec3ab
Mukul Rajput <148499007+190764@users.noreply.github.com>
2024-01-28 05:09:13 +0530

    Column wise sum of matrix

--- ARRAYS/printing.cpp
+++ ARRAYS/printing.cpp
@@ -1,28 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
-void printPairs(vector<int>&arr,int n){
-  for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
-      cout<<"("<<arr[i]<<","<<arr[j]<<")"<<" ";
-    }
-  cout<<endl;
+// void printPairs(vector<int>&arr,int n){
+//   for(int i=0;i<n;i++){
+//     for(int j=0;j<n;j++){
+//       cout<<"("<<arr[i]<<","<<arr[j]<<")"<<" ";
+//     }
+//   cout<<endl;
+//   }
+// }
+
+// void printTriplets(vector<int>&arr,int n){
+//   for(int i=0;i<n;i++){
+//     for(int j=0;j<n;j++){
+//       for(int k=0;k<n;k++){
+//        cout<<"("<<arr[i]<<","<<arr[j]<<","<<arr[k]<<")"<<" ";
+//       }
+//     }
+//   cout<<endl;
+//   }
+// }
+// int main(){
+//   vector <int> arr={1,2,3};
+//   int n=arr.size();
+//   printPairs(arr,n);
+//   // printTriplets(arr,n);
+//   return 0;
+// }
+
+
+
+
+//COLUMN WISE SUM
+
+void columnWiseSum(int arr[][4],int row,int col){
+ 
+ for(int i=0;i<col;i++){int sum=0;
+  for(int j=0;j<row;j++){
+    sum+=arr[j][i];
   }
+  cout<< sum<<" ";
+ }
+ cout<<endl;
 }
 
-void printTriplets(vector<int>&arr,int n){
-  for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
-      for(int k=0;k<n;k++){
-       cout<<"("<<arr[i]<<","<<arr[j]<<","<<arr[k]<<")"<<" ";
-      }
-    }
-  cout<<endl;
-  }
-}
 int main(){
-  vector <int> arr={1,2,3};
-  int n=arr.size();
-  printPairs(arr,n);
-  // printTriplets(arr,n);
+  int arr[][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+  int row=3,col=4;
+   columnWiseSum(arr,row,col);
   return 0;
 }
