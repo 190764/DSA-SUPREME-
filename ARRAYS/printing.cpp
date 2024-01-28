1486f83e34fcbadab759638b4f17e2432d8e3e03
Mukul Rajput <148499007+190764@users.noreply.github.com>
2024-01-28 05:36:04 +0530

    Transpose of a matrix

--- ARRAYS/printing.cpp
+++ ARRAYS/printing.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 // void printPairs(vector<int>&arr,int n){
 //   for(int i=0;i<n;i++){
@@ -27,25 +27,54 @@ using namespace std;
 //   return 0;
 // }
 
+// COLUMN WISE SUM
 
+// void columnWiseSum(int arr[][4],int row,int col){
 
+//  for(int i=0;i<col;i++){int sum=0;
+//   for(int j=0;j<row;j++){
+//     sum+=arr[j][i];
+//   }
+//   cout<< sum<<" ";
+//  }
+//  cout<<endl;
+// }
 
-//COLUMN WISE SUM
+// int main(){
+//   int arr[][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+//   int row=3,col=4;
+//    columnWiseSum(arr,row,col);
+//   return 0;
+// }
 
-void columnWiseSum(int arr[][4],int row,int col){
- 
- for(int i=0;i<col;i++){int sum=0;
-  for(int j=0;j<row;j++){
-    sum+=arr[j][i];
+// TRANSPOSE
+
+void transposeOfMatrix(int arr[4][4], int n, int m)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = i; j < m; j++)
+    {
+    swap(arr[i][j],arr[j][i]);
+    }
   }
-  cout<< sum<<" ";
- }
- cout<<endl;
+
+ for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+    cout<<arr[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+  
 }
 
-int main(){
-  int arr[][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-  int row=3,col=4;
-   columnWiseSum(arr,row,col);
+int main()
+{
+  int arr[][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+  
+  int n = 4, m = 4;
+  transposeOfMatrix(arr,n,m);
   return 0;
 }
